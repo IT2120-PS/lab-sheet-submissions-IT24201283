d98bd44d013607f8598179766443ab96f9f04b42
IT24201283 <it24201283@my.sliit.lk>
2025-09-25 15:19:18 +0530

    Lab 08

---
+++ Lab Sheet 08/it24201283.R
@@ -0,0 +1,31 @@
+setwd("C:/Users/IT24201283/Desktop/it24201283")
+data <-read.table("Exercise - LaptopsWeights.txt" ,header = TRUE)
+fix(data)
+attach(data)
+
+popmn  <- mean(Weight.kg.)
+popmn
+popvar <- var (Weight.kg.)
+popvar
+
+
+samples <- c()
+n <- c()
+
+for(i in 1:25){
+  s <- sample(Weight.kg.,6,replace = TRUE)
+  samples <- cbind(samples,s)
+  n <- c(n , paste('s' , i))
+}
+
+colnames(samples) = n
+
+s.means <- apply(samples ,2, mean)
+s.means
+s.vars <- apply(samples , 2 , var)
+s.vars
+
+samplemean <- mean(s.means)
+samplemean
+samplevars <- var(s.means)
+samplevars
